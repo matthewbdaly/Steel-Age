437e9b0580caf7682f5746ce7352d25b8c86d184
Matthew Daly <matthewbdaly@gmail.com>
2012-02-27 21:47:51 +0000

    Moved the warning to the very top of the page, centred the text, and set the header to force IE to use the most recent version of its rendering engine.

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
     <head>
         <meta http-equiv="Content-Type" content="text/html:charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
         <?php wp_head(); ?>
@@ -9,11 +10,11 @@
             <title><?php bloginfo('name'); ?> <?php wp_title('-'); ?></title>
     </head>
     <body>
+            <!--[if lt IE 8]><p class="warning">You are using an outdated ve    rsion of Internet Explorer. For security reasons you should upgrade your browser (also this site will look a lot nicer!). Please go to Windows Updates and install the latest version.</p><![endif]-->
         <div id="wrapper">
             <div id="header">
             <h1><a href="<?php get_option('home'); ?>"><?php bloginfo('name'); ?></a></h1>
             <h2><?php bloginfo('description'); ?></h2>
-            <!--[if lt IE 8]><p class="warning">You are using an outdated ve    rsion of Internet Explorer. For security reasons you should upgrade your browser (also this site will look a lot nicer!). Please go to Windows Updates and install the latest version.</p><![endif]-->
                 <div id="navBar">
                     <ul>
                         <?php wp_page_menu('show_home=Home'); ?>
