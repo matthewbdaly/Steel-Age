fcff59d51e43561a72e5ba1f2a46117e9511156b
Matthew Daly <matthewbdaly@gmail.com>
2012-02-27 17:35:24 +0000

    Removed the hyperlinks from the profile link text.

--- sidebar.php
+++ sidebar.php
@@ -13,10 +13,10 @@
                         </li>
                         <li>
                             <ul>
-                                <li class="profileLink"><a class="profileLinkImage" href="http://twitter.com/mattbd"><img src="<?php bloginfo('template_directory') ?>/images/twitter.png" alt="Find me on Twitter" title="Find me on Twitter" /></a><a href="http://twitter.com/mattbd" class="profileLinkText" title="Find me on Twitter">Twitter</a></li>
-                                <li class="profileLink"><a class="profileLinkImage" href="http://uk.linkedin.com/in/matthewbdaly"><img src="<?php bloginfo('template_directory') ?>/images/linkedin.png" alt="Find me on LinkedIn" title="Find me on LinkedIn" /></a><a href="http://uk.linkedin.com/in/matthewbdaly" class="profileLinkText" title="Find me on LinkedIn">LinkedIn</a></li>
-                                <li class="profileLink"><a class="profileLinkImage" href="https://github.com/matthewbdaly"><img src="<?php bloginfo('template_directory') ?>/images/github.png" alt="Find me on GitHub" title="Find me on GitHub" /></a><a href="https://github.com/matthewbdaly" class="profileLinkText" title="Find me on GitHub">GitHub</a></li>
-                                <li class="profileLink"><a class="profileLinkImage" href="http://feeds.feedburner.com/MatthewDalysBlog"><img src="<?php bloginfo('template_directory') ?>/images/rss.png" alt="Subscribe to my posts" title="Subscribe to my posts" /></a><a href="http://feeds.feedburner.com/MatthewDalysBlog" class="profileLinkText" title="Subscribe to my posts">Subscribe</a></li>
+                                <li class="profileLink"><a class="profileLinkImage" href="http://twitter.com/mattbd"><img src="<?php bloginfo('template_directory') ?>/images/twitter.png" alt="Find me on Twitter" title="Find me on Twitter" /></a><span class="profileLinkText">Twitter</span></li>
+                                <li class="profileLink"><a class="profileLinkImage" href="http://uk.linkedin.com/in/matthewbdaly"><img src="<?php bloginfo('template_directory') ?>/images/linkedin.png" alt="Find me on LinkedIn" title="Find me on LinkedIn" /></a><span class="profileLinkText">LinkedIn</span></li>
+                                <li class="profileLink"><a class="profileLinkImage" href="https://github.com/matthewbdaly"><img src="<?php bloginfo('template_directory') ?>/images/github.png" alt="Find me on GitHub" title="Find me on GitHub" /></a><span class="profileLinkText">GitHub</span></li>
+                                <li class="profileLink"><a class="profileLinkImage" href="http://feeds.feedburner.com/MatthewDalysBlog"><img src="<?php bloginfo('template_directory') ?>/images/rss.png" alt="Subscribe to my posts" title="Subscribe to my posts" /></a><span class="profileLinkText">Subscribe</span></li>
                             </ul>
                         </li>
                         <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
