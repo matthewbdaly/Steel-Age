c6d7a4fa6a9e3279b8b5e3bbf44dbd5d1a2c4612
Matthew Daly <matthewbdaly@gmail.com>
2012-02-28 15:23:44 +0000

    Gotten the search working.

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" action="<?php echo home_url('/');?>" name="searchform" id="searchform">
                             <fieldset>
-                            <h4><label for="search">Search</label><input name="search" id="s" name="s" type="text" /></h4>
+                            <h4><label for="s">Search</label><input id="s" name="s" type="text" /></h4>
                             </fieldset>
                         </form>
