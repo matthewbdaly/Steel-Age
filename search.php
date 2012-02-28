c82fc268aff4dc7a1605328238d9e1d62a9db367
Matthew Daly <matthewbdaly@gmail.com>
2012-02-28 16:09:00 +0000

    Made the search look a little nicer.

--- search.php
+++ search.php
@@ -3,22 +3,12 @@
                     <div id="content">
                     <?php if (have_posts()) ?>
                         <?php while(have_posts()) : the_post(); ?>
-                        <div class="post">
-                            <h2>
-                                <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
-                                <?php the_title(); ?>
-                                </a>
-                            </h2>
-                            <div class="postContent">
-                            <?php the_content(); ?>
-                            <h6><?php the_time('F jS, Y'); ?> by <?php the_author(); ?> </h6>
-                            </div>
+                        <div class="searchResult">
+                        <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                        <?php the_excerpt(); ?>
+                        <h6><?php the_time('l jS F, Y - g:ia') ?></h6>
                         </div>
                         <?php endwhile; ?>
-                        <div class="navigation">
-                            <div class="alignLeft"><?php previous_posts_link('&laquo; Previous Entries'); ?></div>
-                            <div class="alignRight"><?php next_posts_link('Next Entries &raquo;'); ?></div>
-                        </div>
                     </div>
                 </div>
             </div>
