000b271766b578628a6eba291a9054e5ecc9c8f6
Matthew Daly <matthewbdaly@gmail.com>
2012-02-28 00:00:02 +0000

    searchform.php and page.php created

---
+++ searchpage.php
@@ -0,0 +1,17 @@
+<?php
+/*
+TEmplate Name: Search Page
+ */
+?>
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+                    <div id="content">
+                        <div class="post">
+                            <h2>Search Results</h2>
+                            <?php get_search_form(); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+<?php get_footer(); ?>
